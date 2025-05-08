b84384ddce074682357929454aa9c7b7708bcda1
Bronwyn Taljaard <bronwyntaljaard@bronwyns-laptop.home>
2025-05-08 16:32:18 +0200

    Added user input and edit button

---
+++ src/components/Player.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+
+	function handleEditClick() {
+		setIsEditing((editing) => !editing);
+	}
+
+	return (
+		<li>
+			<span className="player">
+				{!isEditing && <span className="player-name">{name}</span>}
+				{isEditing && <input type="text" required value={name}></input>}
+				<span className="player-symbol">{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+		</li>
+	);
+}
